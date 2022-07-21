8c94cf88a9ee71397b150a20fb9557a6da1a6621
LU Yicheng <shenguosofe@gmail.com>
2022-07-21 19:44:43 +0800

    Update LZMA SDK to version:22.01
    Update LZMA SDK to version:22.01
    LZMASDK CHANGELOGS:
    22.01          2022-07-15
    -------------------------
    - UDF support was improved to UDF version 2.60.
    - HFS and APFS support was improved.
    
    22.00          2022-06-15
    -------------------------
    - 7-Zip now can extract APFS (Apple File System) images that can be used in DMG files.
    - 7-Zip now can create TAR archives in POSIX (pax) tar format with the switches
        -ttar -mm=pax or -ttar -mm=posix
    - 7-Zip now can store additional file timestamps with high precision (1 ns in Linux)
      in tar/pax archives with the following switches:
        -ttar -mm=pax -mtp=3 -mtc -mta
    - New switches for Linux version for TAR archives:
      -snoi : store owner/group ids in archive or set owner/group ids from archive to extracted files.
      -snon : store owner/group names in archive
    - New -snz switch to propagate Zone.Identifier stream to extracted files (Windows).
    - New option "Propagate Zone.Id stream" in Tools/Options/7-Zip menu.
    - New "Options" window in "Add to archive" allows to select what metadata must be
      included to archive. Also it allows to select new option
      "Do not change source files last access time".
    - Some bugs were fixed.
    
    21.07          2021-12-26
    -------------------------
    - 7-Zip now can extract VHDX disk images (Microsoft Hyper-V Virtual Hard Disk v2 format).
    - New switches: -spm and -im!{file_path} to exclude directories from processing
        for specified paths that don't contain path separator character at the end of path.
    - In the "Add to Archive" window, now it is allowed to use -m prefix for "Parameters"
      field as in command line: -mparam.
    - The sorting order of files in archives was slightly changed to be more consistent
      for cases where the name of some directory is the same as the prefix part of the name
      of another directory or file.
    - TAR archives created by 7-Zip now are more consistent with archives created by GNU TAR program.
    
    21.06          2021-11-24
    -------------------------
    - The window "Add to Archive" now allows to set a limit on memory usage (RAM)
      that will be used for compressing.
    - New switch -mmemuse={N}g / -mmemuse=p{N} to set a limit on memory usage (RAM)
      for compressing and decompressing.
    - Bug in versions 21.00-21.05 was fixed:
      7-Zip didn't set attributes of directories during archive extracting.
    - Some bugs were fixed.
    
    -----
    7z_fun.cpp:减少了部分代码量,提高编译器性能

--- installerinterface/lzma2103/CPP/7zip/UI/Client7z/7z_fun.cpp
+++ installerinterface/lzma2103/CPP/7zip/UI/Client7z/7z_fun.cpp
@@ -24,30 +24,42 @@ extern
 HINSTANCE g_hInstance;
 HINSTANCE g_hInstance = 0;
 
-// You can find the list of all GUIDs in Guid.txt file.
-// use another CLSIDs, if you want to support other formats (zip, rar, ...).
-// {23170F69-40C1-278A-1000-000110070000}
+// You can find full list of all GUIDs supported by 7-Zip in Guid.txt file.
+// 7z format GUID: {23170F69-40C1-278A-1000-000110070000}
 
-DEFINE_GUID(CLSID_CFormat7z,
-  0x23170F69, 0x40C1, 0x278A, 0x10, 0x00, 0x00, 0x01, 0x10, 0x07, 0x00, 0x00);
-DEFINE_GUID(CLSID_CFormatXz,
-  0x23170F69, 0x40C1, 0x278A, 0x10, 0x00, 0x00, 0x01, 0x10, 0x0C, 0x00, 0x00);
+#define DEFINE_GUID_ARC(name, id) DEFINE_GUID(name, \
+  0x23170F69, 0x40C1, 0x278A, 0x10, 0x00, 0x00, 0x01, 0x10, id, 0x00, 0x00);
 
-#define CLSID_Format CLSID_CFormat7z
-// #define CLSID_Format CLSID_CFormatXz
+enum
+{
+    kId_Zip = 1,
+    kId_BZip2 = 2,
+    kId_7z = 7,
+    kId_Xz = 0xC,
+    kId_Tar = 0xEE,
+    kId_GZip = 0xEF
+};
+
+// use another id, if you want to support other formats (zip, Xz, ...).
+// DEFINE_GUID_ARC (CLSID_Format, kId_Zip)
+// DEFINE_GUID_ARC (CLSID_Format, kId_BZip2)
+// DEFINE_GUID_ARC (CLSID_Format, kId_Xz)
+// DEFINE_GUID_ARC (CLSID_Format, kId_Tar)
+// DEFINE_GUID_ARC (CLSID_Format, kId_GZip)
+DEFINE_GUID_ARC(CLSID_Format, kId_7z)
 
 using namespace NWindows;
 using namespace NFile;
 using namespace NDir;
-static const char * const kCopyrightString =
-  "\n"
-  "7-Zip"
-  " (" kDllName " client)"
-  " " MY_VERSION
-  " : " MY_COPYRIGHT_DATE
-  "\n";
+static const char* const kCopyrightString =
+"\n"
+"7-Zip"
+" (" kDllName " client)"
+" " MY_VERSION
+" : " MY_COPYRIGHT_DATE
+"\n";
 
-static const char * const kHelpString =
+static const char* const kHelpString =
 "Usage: 7zcl.exe [a | l | x] archive.7z [fileName ...]\n"
 "Examples:\n"
 "  7zcl.exe a archive.7z f1.txt f2.txt  : compress two files to archive.7z\n"
@@ -55,162 +67,240 @@ static const char * const kHelpString =
 "  7zcl.exe x archive.7z   : eXtract files from archive.7z\n";
 
 
-static void Convert_UString_to_AString(const UString &s, AString &temp)
+static void Convert_UString_to_AString(const UString& s, AString& temp)
 {
-  int codePage = CP_OEMCP;
-  /*
-  int g_CodePage = -1;
-  int codePage = g_CodePage;
-  if (codePage == -1)
-    codePage = CP_OEMCP;
-  if (codePage == CP_UTF8)
-    ConvertUnicodeToUTF8(s, temp);
-  else
-  */
+    int codePage = CP_OEMCP;
+    /*
+    int g_CodePage = -1;
+    int codePage = g_CodePage;
+    if (codePage == -1)
+      codePage = CP_OEMCP;
+    if (codePage == CP_UTF8)
+      ConvertUnicodeToUTF8(s, temp);
+    else
+    */
     UnicodeStringToMultiByte2(temp, s, (UINT)codePage);
 }
 
-static FString CmdStringToFString(const char *s)
+static FString CmdStringToFString(const char* s)
 {
-  return us2fs(GetUnicodeString(s));
+    return us2fs(GetUnicodeString(s));
 }
 
-static void Print(const char *s)
+static void Print(const char* s)
 {
-  fputs(s, stdout);
+    fputs(s, stdout);
 }
 
-static void Print(const AString &s)
+static void Print(const AString& s)
 {
-  Print(s.Ptr());
+    Print(s.Ptr());
 }
 
-static void Print(const UString &s)
+static void Print(const UString& s)
 {
-  AString as;
-  Convert_UString_to_AString(s, as);
-  Print(as);
+    AString as;
+    Convert_UString_to_AString(s, as);
+    Print(as);
 }
 
-static void Print(const wchar_t *s)
+static void Print(const wchar_t* s)
 {
-  Print(UString(s));
+    Print(UString(s));
 }
 
 static void PrintNewLine()
 {
-  Print("\n");
+    Print("\n");
 }
 
-static void PrintStringLn(const char *s)
+static void PrintStringLn(const char* s)
 {
-  Print(s);
-  PrintNewLine();
+    Print(s);
+    PrintNewLine();
 }
 
-static void PrintError(const char *message)
+static void PrintError(const char* message)
 {
-  Print("Error: ");
-  PrintNewLine();
-  Print(message);
-  PrintNewLine();
+    Print("Error: ");
+    PrintNewLine();
+    Print(message);
+    PrintNewLine();
 }
 
-static void PrintError(const char *message, const FString &name)
+static void PrintError(const char* message, const FString& name)
 {
-  PrintError(message);
-  Print(name);
+    PrintError(message);
+    Print(name);
 }
 
 
-static HRESULT IsArchiveItemProp(IInArchive *archive, UInt32 index, PROPID propID, bool &result)
+static HRESULT IsArchiveItemProp(IInArchive* archive, UInt32 index, PROPID propID, bool& result)
 {
-  NCOM::CPropVariant prop;
-  RINOK(archive->GetProperty(index, propID, &prop));
-  if (prop.vt == VT_BOOL)
-    result = VARIANT_BOOLToBool(prop.boolVal);
-  else if (prop.vt == VT_EMPTY)
-    result = false;
-  else
-    return E_FAIL;
-  return S_OK;
+    NCOM::CPropVariant prop;
+    RINOK(archive->GetProperty(index, propID, &prop));
+    if (prop.vt == VT_BOOL)
+        result = VARIANT_BOOLToBool(prop.boolVal);
+    else if (prop.vt == VT_EMPTY)
+        result = false;
+    else
+        return E_FAIL;
+    return S_OK;
 }
 
-static HRESULT IsArchiveItemFolder(IInArchive *archive, UInt32 index, bool &result)
+static HRESULT IsArchiveItemFolder(IInArchive* archive, UInt32 index, bool& result)
 {
-  return IsArchiveItemProp(archive, index, kpidIsDir, result);
+    return IsArchiveItemProp(archive, index, kpidIsDir, result);
 }
 
 
-static const wchar_t * const kEmptyFileAlias = L"[Content]";
+static const wchar_t* const kEmptyFileAlias = L"[Content]";
 
 
 //////////////////////////////////////////////////////////////
 // Archive Open callback class
 
 
-class CArchiveOpenCallback:
-  public IArchiveOpenCallback,
-  public ICryptoGetTextPassword,
-  public CMyUnknownImp
+class CArchiveOpenCallback :
+    public IArchiveOpenCallback,
+    public ICryptoGetTextPassword,
+    public CMyUnknownImp
 {
 public:
-  MY_UNKNOWN_IMP1(ICryptoGetTextPassword)
+    MY_UNKNOWN_IMP1(ICryptoGetTextPassword)
 
-  STDMETHOD(SetTotal)(const UInt64 *files, const UInt64 *bytes);
-  STDMETHOD(SetCompleted)(const UInt64 *files, const UInt64 *bytes);
+        STDMETHOD(SetTotal)(const UInt64* files, const UInt64* bytes);
+    STDMETHOD(SetCompleted)(const UInt64* files, const UInt64* bytes);
 
-  STDMETHOD(CryptoGetTextPassword)(BSTR *password);
+    STDMETHOD(CryptoGetTextPassword)(BSTR* password);
 
-  bool PasswordIsDefined;
-  UString Password;
+    bool PasswordIsDefined;
+    UString Password;
 
-  CArchiveOpenCallback() : PasswordIsDefined(false) {}
+    CArchiveOpenCallback() : PasswordIsDefined(false) {}
 };
 
-STDMETHODIMP CArchiveOpenCallback::SetTotal(const UInt64 * /* files */, const UInt64 * /* bytes */)
+STDMETHODIMP CArchiveOpenCallback::SetTotal(const UInt64* /* files */, const UInt64* /* bytes */)
 {
-  return S_OK;
+    return S_OK;
 }
 
-STDMETHODIMP CArchiveOpenCallback::SetCompleted(const UInt64 * /* files */, const UInt64 * /* bytes */)
+STDMETHODIMP CArchiveOpenCallback::SetCompleted(const UInt64* /* files */, const UInt64* /* bytes */)
 {
-  return S_OK;
-}
-  
-STDMETHODIMP CArchiveOpenCallback::CryptoGetTextPassword(BSTR *password)
-{
-  if (!PasswordIsDefined)
-  {
-    // You can ask real password here from user
-    // Password = GetPassword(OutStream);
-    // PasswordIsDefined = true;
-    PrintError("Password is not defined");
-    return E_ABORT;
-  }
-  return StringToBstr(Password, password);
+    return S_OK;
 }
 
+STDMETHODIMP CArchiveOpenCallback::CryptoGetTextPassword(BSTR* password)
+{
+    if (!PasswordIsDefined)
+    {
+        // You can ask real password here from user
+        // Password = GetPassword(OutStream);
+        // PasswordIsDefined = true;
+        PrintError("Password is not defined");
+        return E_ABORT;
+    }
+    return StringToBstr(Password, password);
+}
 
 
-static const char * const kIncorrectCommand = "incorrect command";
+
+static const char* const kIncorrectCommand = "incorrect command";
 
 //////////////////////////////////////////////////////////////
 // Archive Extracting callback class
 
-static const char * const kTestingString    =  "Testing     ";
-static const char * const kExtractingString =  "Extracting  ";
-static const char * const kSkippingString   =  "Skipping    ";
+static const char* const kTestingString = "Testing     ";
+static const char* const kExtractingString = "Extracting  ";
+static const char* const kSkippingString = "Skipping    ";
+static const char* const kReadingString = "Reading     ";
 
-static const char * const kUnsupportedMethod = "Unsupported Method";
-static const char * const kCRCFailed = "CRC Failed";
-static const char * const kDataError = "Data Error";
-static const char * const kUnavailableData = "Unavailable data";
-static const char * const kUnexpectedEnd = "Unexpected end of data";
-static const char * const kDataAfterEnd = "There are some data after the end of the payload data";
-static const char * const kIsNotArc = "Is not archive";
-static const char * const kHeadersError = "Headers Error";
+static const char* const kUnsupportedMethod = "Unsupported Method";
+static const char* const kCRCFailed = "CRC Failed";
+static const char* const kDataError = "Data Error";
+static const char* const kUnavailableData = "Unavailable data";
+static const char* const kUnexpectedEnd = "Unexpected end of data";
+static const char* const kDataAfterEnd = "There are some data after the end of the payload data";
+static const char* const kIsNotArc = "Is not archive";
+static const char* const kHeadersError = "Headers Error";
 
+struct CArcTime
+{
+    FILETIME FT;
+    UInt16 Prec;
+    Byte Ns100;
+    bool Def;
+
+    CArcTime()
+    {
+        Clear();
+    }
+
+    void Clear()
+    {
+        FT.dwHighDateTime = FT.dwLowDateTime = 0;
+        Prec = 0;
+        Ns100 = 0;
+        Def = false;
+    }
+
+    bool IsZero() const
+    {
+        return FT.dwLowDateTime == 0 && FT.dwHighDateTime == 0 && Ns100 == 0;
+    }
+
+    int GetNumDigits() const
+    {
+        if (Prec == k_PropVar_TimePrec_Unix ||
+            Prec == k_PropVar_TimePrec_DOS)
+            return 0;
+        if (Prec == k_PropVar_TimePrec_HighPrec)
+            return 9;
+        if (Prec == k_PropVar_TimePrec_0)
+            return 7;
+        int digits = (int)Prec - (int)k_PropVar_TimePrec_Base;
+        if (digits < 0)
+            digits = 0;
+        return digits;
+    }
+
+    void Write_To_FiTime(CFiTime& dest) const
+    {
+#ifdef _WIN32
+        dest = FT;
+#else
+        if (FILETIME_To_timespec(FT, dest))
+            if ((Prec == k_PropVar_TimePrec_Base + 8 ||
+                Prec == k_PropVar_TimePrec_Base + 9)
+                && Ns100 != 0)
+            {
+                dest.tv_nsec += Ns100;
+            }
+#endif
+    }
+
+    void Set_From_Prop(const PROPVARIANT& prop)
+    {
+        FT = prop.filetime;
+        unsigned prec = 0;
+        unsigned ns100 = 0;
+        const unsigned prec_Temp = prop.wReserved1;
+        if (prec_Temp != 0
+            && prec_Temp <= k_PropVar_TimePrec_1ns
+            && prop.wReserved3 == 0)
+        {
+            const unsigned ns100_Temp = prop.wReserved2;
+            if (ns100_Temp < 100)
+            {
+                ns100 = ns100_Temp;
+                prec = prec_Temp;
+            }
+        }
+        Prec = (UInt16)prec;
+        Ns100 = (Byte)ns100;
+        Def = true;
+    }
+};
 
 class CArchiveExtractCallback:
   public IArchiveExtractCallback,
@@ -240,11 +330,10 @@ private:
   bool _extractMode;
   struct CProcessedFileInfo
   {
-    FILETIME MTime;
+    CArcTime MTime;
     UInt32 Attrib;
     bool isDir;
-    bool AttribDefined;
-    bool MTimeDefined;
+    bool Attrib_Defined;
   } _processedFileInfo;
 
   COutFileStream *_outFileStreamSpec;
@@ -317,32 +406,31 @@ STDMETHODIMP CArchiveExtractCallback::GetStream(UInt32 index,
     if (prop.vt == VT_EMPTY)
     {
       _processedFileInfo.Attrib = 0;
-      _processedFileInfo.AttribDefined = false;
+      _processedFileInfo.Attrib_Defined = false;
     }
     else
     {
       if (prop.vt != VT_UI4)
         return E_FAIL;
       _processedFileInfo.Attrib = prop.ulVal;
-      _processedFileInfo.AttribDefined = true;
+      _processedFileInfo.Attrib_Defined = true;
     }
   }
 
   RINOK(IsArchiveItemFolder(_archiveHandler, index, _processedFileInfo.isDir));
 
   {
+    _processedFileInfo.MTime.Clear();
     // Get Modified Time
     NCOM::CPropVariant prop;
     RINOK(_archiveHandler->GetProperty(index, kpidMTime, &prop));
-    _processedFileInfo.MTimeDefined = false;
     switch (prop.vt)
     {
       case VT_EMPTY:
         // _processedFileInfo.MTime = _utcMTimeDefault;
         break;
       case VT_FILETIME:
-        _processedFileInfo.MTime = prop.filetime;
-        _processedFileInfo.MTimeDefined = true;
+          _processedFileInfo.MTime.Set_From_Prop(prop);
         break;
       default:
         return E_FAIL;
@@ -469,12 +557,16 @@ STDMETHODIMP CArchiveExtractCallback::SetOperationResult(Int32 operationResult)
 
   if (_outFileStream)
   {
-    if (_processedFileInfo.MTimeDefined)
-      _outFileStreamSpec->SetMTime(&_processedFileInfo.MTime);
+      if (_processedFileInfo.MTime.Def)
+      {
+          CFiTime ft;
+          _processedFileInfo.MTime.Write_To_FiTime(ft);
+          _outFileStreamSpec->SetMTime(&ft);
+      }
     RINOK(_outFileStreamSpec->Close());
   }
   _outFileStream.Release();
-  if (_extractMode && _processedFileInfo.AttribDefined)
+  if (_extractMode && _processedFileInfo.Attrib_Defined)
     SetFileAttrib_PosixHighDetect(_diskFilePath, _processedFileInfo.Attrib);
   PrintNewLine();
   return S_OK;
@@ -499,136 +591,15 @@ STDMETHODIMP CArchiveExtractCallback::CryptoGetTextPassword(BSTR *password)
 //////////////////////////////////////////////////////////////
 // Archive Creating callback class
 
-struct CDirItem
+struct CDirItem : public NWindows::NFile::NFind::CFileInfoBase
 {
-  UInt64 Size;
-  FILETIME CTime;
-  FILETIME ATime;
-  FILETIME MTime;
-  UString Name;
-  FString FullPath;
-  UInt32 Attrib;
+    UString Path_For_Handler;
+    FString FullPath; // for filesystem
 
-  bool isDir() const { return (Attrib & FILE_ATTRIBUTE_DIRECTORY) != 0 ; }
+    CDirItem(const NWindows::NFile::NFind::CFileInfo& fi) :
+        CFileInfoBase(fi)
+    {}
 };
-
-class CArchiveUpdateCallback:
-  public IArchiveUpdateCallback2,
-  public ICryptoGetTextPassword2,
-  public CMyUnknownImp
-{
-public:
-  MY_UNKNOWN_IMP2(IArchiveUpdateCallback2, ICryptoGetTextPassword2)
-
-  // IProgress
-  STDMETHOD(SetTotal)(UInt64 size);
-  STDMETHOD(SetCompleted)(const UInt64 *completeValue);
-
-  // IUpdateCallback2
-  STDMETHOD(GetUpdateItemInfo)(UInt32 index,
-      Int32 *newData, Int32 *newProperties, UInt32 *indexInArchive);
-  STDMETHOD(GetProperty)(UInt32 index, PROPID propID, PROPVARIANT *value);
-  STDMETHOD(GetStream)(UInt32 index, ISequentialInStream **inStream);
-  STDMETHOD(SetOperationResult)(Int32 operationResult);
-  STDMETHOD(GetVolumeSize)(UInt32 index, UInt64 *size);
-  STDMETHOD(GetVolumeStream)(UInt32 index, ISequentialOutStream **volumeStream);
-
-  STDMETHOD(CryptoGetTextPassword2)(Int32 *passwordIsDefined, BSTR *password);
-
-public:
-  CRecordVector<UInt64> VolumesSizes;
-  UString VolName;
-  UString VolExt;
-
-  FString DirPrefix;
-  const CObjectVector<CDirItem> *DirItems;
-
-  bool PasswordIsDefined;
-  UString Password;
-  bool AskPassword;
-
-  bool m_NeedBeClosed;
-
-  FStringVector FailedFiles;
-  CRecordVector<HRESULT> FailedCodes;
-
-  CArchiveUpdateCallback():
-      DirItems(NULL),
-      PasswordIsDefined(false),
-      AskPassword(false)
-      {}
-
-  ~CArchiveUpdateCallback() { Finilize(); }
-  HRESULT Finilize();
-
-  void Init(const CObjectVector<CDirItem> *dirItems)
-  {
-    DirItems = dirItems;
-    m_NeedBeClosed = false;
-    FailedFiles.Clear();
-    FailedCodes.Clear();
-  }
-};
-
-STDMETHODIMP CArchiveUpdateCallback::SetTotal(UInt64 /* size */)
-{
-  return S_OK;
-}
-
-STDMETHODIMP CArchiveUpdateCallback::SetCompleted(const UInt64 * /* completeValue */)
-{
-  return S_OK;
-}
-
-STDMETHODIMP CArchiveUpdateCallback::GetUpdateItemInfo(UInt32 /* index */,
-      Int32 *newData, Int32 *newProperties, UInt32 *indexInArchive)
-{
-  if (newData)
-    *newData = BoolToInt(true);
-  if (newProperties)
-    *newProperties = BoolToInt(true);
-  if (indexInArchive)
-    *indexInArchive = (UInt32)(Int32)-1;
-  return S_OK;
-}
-
-STDMETHODIMP CArchiveUpdateCallback::GetProperty(UInt32 index, PROPID propID, PROPVARIANT *value)
-{
-  NCOM::CPropVariant prop;
-  
-  if (propID == kpidIsAnti)
-  {
-    prop = false;
-    prop.Detach(value);
-    return S_OK;
-  }
-
-  {
-    const CDirItem &dirItem = (*DirItems)[index];
-    switch (propID)
-    {
-      case kpidPath:  prop = dirItem.Name; break;
-      case kpidIsDir:  prop = dirItem.isDir(); break;
-      case kpidSize:  prop = dirItem.Size; break;
-      case kpidAttrib:  prop = dirItem.Attrib; break;
-      case kpidCTime:  prop = dirItem.CTime; break;
-      case kpidATime:  prop = dirItem.ATime; break;
-      case kpidMTime:  prop = dirItem.MTime; break;
-    }
-  }
-  prop.Detach(value);
-  return S_OK;
-}
-
-HRESULT CArchiveUpdateCallback::Finilize()
-{
-  if (m_NeedBeClosed)
-  {
-    PrintNewLine();
-    m_NeedBeClosed = false;
-  }
-  return S_OK;
-}
 
 static void GetStream2(const wchar_t *name)
 {
@@ -636,91 +607,6 @@ static void GetStream2(const wchar_t *name)
   if (name[0] == 0)
     name = kEmptyFileAlias;
   Print(name);
-}
-
-STDMETHODIMP CArchiveUpdateCallback::GetStream(UInt32 index, ISequentialInStream **inStream)
-{
-  RINOK(Finilize());
-
-  const CDirItem &dirItem = (*DirItems)[index];
-  GetStream2(dirItem.Name);
- 
-  if (dirItem.isDir())
-    return S_OK;
-
-  {
-    CInFileStream *inStreamSpec = new CInFileStream;
-    CMyComPtr<ISequentialInStream> inStreamLoc(inStreamSpec);
-    FString path = DirPrefix + dirItem.FullPath;
-    if (!inStreamSpec->Open(path))
-    {
-      DWORD sysError = ::GetLastError();
-      FailedCodes.Add(sysError);
-      FailedFiles.Add(path);
-      // if (systemError == ERROR_SHARING_VIOLATION)
-      {
-        PrintNewLine();
-        PrintError("WARNING: can't open file");
-        // Print(NError::MyFormatMessageW(systemError));
-        return S_FALSE;
-      }
-      // return sysError;
-    }
-    *inStream = inStreamLoc.Detach();
-  }
-  return S_OK;
-}
-
-STDMETHODIMP CArchiveUpdateCallback::SetOperationResult(Int32 /* operationResult */)
-{
-  m_NeedBeClosed = true;
-  return S_OK;
-}
-
-STDMETHODIMP CArchiveUpdateCallback::GetVolumeSize(UInt32 index, UInt64 *size)
-{
-  if (VolumesSizes.Size() == 0)
-    return S_FALSE;
-  if (index >= (UInt32)VolumesSizes.Size())
-    index = VolumesSizes.Size() - 1;
-  *size = VolumesSizes[index];
-  return S_OK;
-}
-
-STDMETHODIMP CArchiveUpdateCallback::GetVolumeStream(UInt32 index, ISequentialOutStream **volumeStream)
-{
-  wchar_t temp[16];
-  ConvertUInt32ToString(index + 1, temp);
-  UString res = temp;
-  while (res.Len() < 2)
-    res.InsertAtFront(L'0');
-  UString fileName = VolName;
-  fileName += '.';
-  fileName += res;
-  fileName += VolExt;
-  COutFileStream *streamSpec = new COutFileStream;
-  CMyComPtr<ISequentialOutStream> streamLoc(streamSpec);
-  if (!streamSpec->Create(us2fs(fileName), false))
-    return ::GetLastError();
-  *volumeStream = streamLoc.Detach();
-  return S_OK;
-}
-
-STDMETHODIMP CArchiveUpdateCallback::CryptoGetTextPassword2(Int32 *passwordIsDefined, BSTR *password)
-{
-  if (!PasswordIsDefined)
-  {
-    if (AskPassword)
-    {
-      // You can ask real password here from user
-      // Password = GetPassword(OutStream);
-      // PasswordIsDefined = true;
-      PrintError("Password is not defined");
-      return E_ABORT;
-    }
-  }
-  *passwordIsDefined = BoolToInt(PasswordIsDefined);
-  return StringToBstr(Password, password);
 }
 
 #if defined(_UNICODE) && !defined(_WIN64) && !defined(UNDER_CE)
